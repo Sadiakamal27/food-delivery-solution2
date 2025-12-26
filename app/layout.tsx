399cd92d494c1ef2eaac19ae51a60e2fe4c586a2
dotplato <iamdotplato@gmail.com>
2025-12-26 21:31:17 +0500

    gtm added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ export const dynamic = "force-dynamic";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap', // optional, helps with font loading behavior
@@ -22,8 +23,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" >
+      <head>
+    {/* Google Tag Manager */}
+    <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WXLFJCBW');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={poppins.className}>
-            <div className="flex min-h-screen flex-col">{children}</div>
+    
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXLFJCBW"
+height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <div className="flex min-h-screen flex-col">{children}</div>
       </body>
     </html>
   );
